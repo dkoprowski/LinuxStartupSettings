3a2678ecff236240d538395fe7a872f92cff3f8c
Daniel Koprowski <dkoprowski@outlook.com>
2016-04-04 12:23:28 +0200

    Added active branch name

--- mycofig.sh
+++ mycofig.sh
@@ -2,11 +2,16 @@
 
 echo "# =====| CUSTOM SECTION |=====" >> ~/.bashrc;
 
+echo "parse_git_branch()" >> ~/.bashrc;
+echo "{" >> ~/.bashrc;
+echo "        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'" >> ~/.bashrc;
+echo "}" >> ~/.bashrc;
+
 echo "alias ll='ls -alF'" >> ~/.bashrc;
 echo "alias la='ls -A'"  >> ~/.bashrc;
 echo "alias l='ls -CF'" >> ~/.bashrc;
 
 echo '# Custom bash prompt via kirsle.net/wizards/ps1.html' >> ~/.bashrc;
-echo 'export PS1="\[$(tput bold)\]\[$(tput setaf 1)\]\n---- \[$(tput bold)\]\[$(tput setaf 7)\]\u@\h\[$(tput setaf 1)\]\[$(tput setaf 1)\] (\t) \[$(tput setaf 7)\]\w\[$(tput setaf 1)\] ----\[$(tput setaf 4)\]\nK0P3R: \[$(tput sgr0)\]"' >> ~/.bashrc;
+echo 'export PS1="\[$(tput bold)\]\[$(tput setaf 1)\]\n---- \[$(tput bold)\]\[$(tput setaf 7)\]\u@\h\[$(tput setaf 1)\]\[$(tput setaf 1)\] (\t) \[$(tput setaf 7)\]\w\[$(tput setaf 1)\] ----\[$(tput setaf 4)\]\n\u\$(parse_git_branch): \[$(tput sgr0)\]"' >> ~/.bashrc;
 . ~/.bashrc;
 
